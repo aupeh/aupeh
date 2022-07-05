848eee546e744622e5889f9c02a969549d75966c
aupeh <108740300+aupeh@users.noreply.github.com>
2022-07-05 06:21:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//for showing and collapsing navbar
+
+function dropNavigation() {
+    document.getElementById('drop-down-nav').style.display = 'flex';
+}
+
+function hideNavigation() {
+    document.getElementById('drop-down-nav').style.display = 'none';
+}
